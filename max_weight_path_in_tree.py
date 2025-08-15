b1a2075266b54e7ce954467940c22ea51d332037
Jakub-Karczewski <66319795+Jakub-Karczewski@users.noreply.github.com>
2025-08-15 23:48:28 +0200

    Update max_weight_path_in_tree.py

--- max_weight_path_in_tree.py
+++ max_weight_path_in_tree.py
@@ -1,35 +1,34 @@
 n = int(input())
 T = [[] for _ in range(n)]
-for i in range(n-1):
+for i in range(n - 1):
     a = int(input())
     b = int(input())
-    c = int(input())
+    c = int(input())  # weights can be negative
     T[a].append([b, c])
     T[b].append([a, c])
 DP = [-1] * n
 calc = [0] * n
-def goodpath(w, prev):
-    #print(w)
-    if(len(T[w]) == 1 and T[w][0][0] == prev):
-        return 0
-    if(DP[w] != -1):
+
+def good_path(w, prev):
+    print(w)
+    if DP[w] != -1:
         return DP[w]
-    m1, m2 = 0, 0
+    m1, m2 = 0, 0  # We are searching for 2 largest (in terms of weight of single going path) descendants of a vertex
     for i in range(len(T[w])):
         v = T[w][i][0]
-        if(v != prev):
-            res = T[w][i][1] + goodpath(v, w)
-            if(res > m1):
+        if v != prev:
+            res = max(0, T[w][i][1] + max(0, good_path(v, w)))  # max (0, ...) is in case if we do not want to go deeper, because the path weight is negative
+            if res > m1:
                 m2 = m1
                 m1 = res
-            elif(res > m2):
+            elif res > m2:
                 m2 = res
     maxi = m1
-    DP[w] = maxi
-    calc[w] = m1 + m2
-    #print(w, DP[w], calc[w])
+    DP[w] = maxi  # case if we want to prolong the path "higher" in a tree and end it in a higher subtree
+    calc[w] = m1 + m2  # case in which the path ends in this subtree
     return maxi
-x = goodpath(0, -1)
+
+x = good_path(0, -1)
 wyn = 0
 for i in range(n):
     wyn = max(wyn, calc[i])
